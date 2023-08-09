f3640a6e43cfba2bf41df7288e92c8d429d982ec
Sethumadhavan Kochukrishnan potti <sethumadhavankochukrishnanpotti@Sethumadhavans-MacBook-Pro.local>
2023-08-09 13:45:52 +1000

    add

--- server/index.d.ts
+++ server/index.d.ts
@@ -1,6 +1,6 @@
 import { Express } from 'express-serve-static-core';
 
-import { IUserModel } from './api/models/User';
+import { IUserModel } from './src/models/User';
 //  This same code I have reproduced on the files that required an extended req.user is required
 declare module 'express-serve-static-core' {
     interface Request {
