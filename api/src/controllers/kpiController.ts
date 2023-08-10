99c7324b284dce6e4075d916be0acd4585f34f0d
Sethumadhavan Kochukrishnan potti <sethumadhavankochukrishnanpotti@Sethumadhavans-MacBook-Pro.local>
2023-08-10 11:55:25 +1000

    add

--- api/src/controllers/kpiController.ts
+++ api/src/controllers/kpiController.ts
@@ -3,5 +3,7 @@ import KPI from '../models/KPI';
 
 export const getKpi = async (req: Request, res: Response, next:NextFunction) => {
     const kpis = await KPI.find();
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
     res.status(200).json(kpis);
 };
