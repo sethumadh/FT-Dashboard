ff77bc701578c12fcdceb83114b4c8759a290ed7
Sethumadhavan Kochukrishnan potti <sethumadhavankochukrishnanpotti@Sethumadhavans-MacBook-Pro.local>
2023-08-10 13:28:10 +1000

    add public file to frntend

--- api/index.ts
+++ api/index.ts
@@ -54,7 +54,7 @@ app.use('/api/student-course', studentCourseRoutes);
 // Server frontend static assets and handle catch-all route
 if (process.env.NODE_ENV === 'production') {
     const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+    app.use(express.static(path.join(__dirname, '/frontend/dist/index.html')));
 
     app.get('*', (req: Request, res: Response) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
 }
